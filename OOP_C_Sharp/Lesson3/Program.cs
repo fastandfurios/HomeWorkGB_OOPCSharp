4111825d5b264c101c713759cf21f6276cd9efd3
Дмитрий <semu-dima@yandex.ru>
2021-11-05 23:17:20 +0300

    create branch 6, checkout 3 -> 6

--- OOP_C_Sharp/Lesson3/Program.cs
+++ OOP_C_Sharp/Lesson3/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Lesson3.Task_1;
-using Lesson3.Task_2;
-using Test_Star = Lesson3.Task_Star.Test;
 
 namespace Lesson3
 {
@@ -11,15 +9,7 @@ namespace Lesson3
         static void Main(string[] args)
         {
             //Задача 1
-            //var test = new Test();
-            //test.Run();
-
-            //Задача 2
-            //var decision = new Decision();
-            //Console.WriteLine(decision.FlipLetters("Привет мир!"));
-
-            //Задача со звездочкой
-            var test = new Test_Star();
+            var test = new Test();
             test.Run();
         }
     }
