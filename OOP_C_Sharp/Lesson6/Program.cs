b3ac756043a28edd2a298c490ae373527d455d49
Дмитрий <semu-dima@yandex.ru>
2021-11-05 23:29:20 +0300

    added new project

---
+++ OOP_C_Sharp/Lesson6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Lesson6.Task_1;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            var test = new Test();
+            test.Run();
+        }
+    }
+}
---
+++ OOP_C_Sharp/Lesson6/Task_1/Test.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson6.Task_1;
+
+namespace Lesson6.Task_1
+{
+    internal sealed class Test
+    {
+        public void Run()
+        {
+            var account1 = new BankAccount { IdAccount = 1 };
+            account1.TypeAccount = nameof(TypesAccount.сберегательный);
+
+            Console.WriteLine("Счет успешно создан!");
+            OutputChangedAccount(account1);
+
+            Console.WriteLine();
+
+            var account2 = new BankAccount { IdAccount = 2 };
+            account2.TypeAccount = nameof(TypesAccount.кредитный);
+
+            Console.WriteLine("Счет успешно создан!");
+            OutputChangedAccount(account2);
+
+            Console.WriteLine();
+
+            account1.DepositAccount(500);
+
+            Console.WriteLine("Счет пополнен!");
+            OutputChangedAccount(account1);
+            
+            Console.WriteLine();
+
+            account1.Transfer(account2, 50);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Текущий статус счета: ");
+            OutputChangedAccount(account1);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Текущий статус счета: ");
+            OutputChangedAccount(account2);
+        }
+
+        private void OutputChangedAccount(BankAccount account)
+            => Console.WriteLine($"Номер счета: {account.IdAccount}\nБаланс счета: {account.BalanceAccount} ед.\nТип счета: {account.TypeAccount}");
+    }
+}
