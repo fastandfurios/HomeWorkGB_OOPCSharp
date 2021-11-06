113c85fec58c8ab53f0257c670b5e57753d45885
Дмитрий <semu-dima@yandex.ru>
2021-11-06 18:43:23 +0300

    added IEquatable<T>

--- OOP_C_Sharp/Lesson6/Task_1/BankAccount_Operators.cs
+++ OOP_C_Sharp/Lesson6/Task_1/BankAccount_Operators.cs
@@ -6,22 +6,36 @@ using System.Threading.Tasks;
 
 namespace Lesson6.Task_1
 {
-    internal sealed partial class BankAccount
+    internal sealed partial class BankAccount : IEquatable<BankAccount>
     {
         public static bool operator ==(BankAccount a, BankAccount b)
-            => a!.IdAccount.Equals(b!.IdAccount) && 
-               a._balanceAccount.Equals(b._balanceAccount) &&
-               a._typeAccount.Equals(b._typeAccount);
+            => a.IdAccount == b.IdAccount && 
+               a._balanceAccount == b._balanceAccount &&
+               a._typeAccount == b._typeAccount;
 
         public static bool operator !=(BankAccount a, BankAccount b)
-            => !(a!.IdAccount.Equals(b!.IdAccount) &&
-               a._balanceAccount.Equals(b._balanceAccount) &&
-               a._typeAccount.Equals(b._typeAccount));
+            => a.IdAccount != b.IdAccount &&
+               a._balanceAccount != b._balanceAccount &&
+               a._typeAccount != b._typeAccount;
 
-        public override bool Equals(object obj) 
-            => obj!.ToString() == ToString();
+        public override bool Equals(object obj) => Equals(obj as BankAccount);
 
-        public override int GetHashCode() 
-            => ToString()!.GetHashCode();
+        public bool Equals(BankAccount other)
+        {
+            if (other is null) return false;
+            return IdAccount == other.IdAccount
+                   && _balanceAccount == other._balanceAccount
+                   && _typeAccount == other._typeAccount;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hash = _idAccount.GetHashCode();
+                hash = (hash * 404) ^ _typeAccount.GetHashCode();
+                return hash;
+            }
+        }
     }
 }
