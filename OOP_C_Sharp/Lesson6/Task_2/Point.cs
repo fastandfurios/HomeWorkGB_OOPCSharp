7e7369635a805553533a04d60caca19b193b8b61
Дмитрий <semu-dima@yandex.ru>
2021-11-10 14:53:36 +0300

    переопределил методы базового класса для точки

--- OOP_C_Sharp/Lesson6/Task_2/Point.cs
+++ OOP_C_Sharp/Lesson6/Task_2/Point.cs
@@ -9,7 +9,14 @@ namespace Lesson6.Task_2
     public class Point : Figure
     {
         private (double x, double y) _coordinates;
-        public override string ToString() => $"({_coordinates.x}:{_coordinates.y})";
+        private ConsoleColor _color;
+        private bool _visible;
+
+        public override string ToString() => $"Точка с координатами ({_coordinates.x}:{_coordinates.y}), цвет {_color}, видимость {_visible}";
+        public override void AxisShift(int x = 0, int y = 0) => _coordinates = (_coordinates.x + x, _coordinates.y + y);
+
         public virtual (double X, double Y) Coordinates { set => _coordinates = value; }
+        public override ConsoleColor Color { set => _color = value; }
+        public override bool Visible { set => _visible = value; }
     }
 }
